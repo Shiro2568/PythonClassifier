c8b5ba6264e6ead30d3d096d6d95e884cfdbfa09
shiro2568 <shiro_navalny01@mail.ru>
2023-05-03 03:24:08 +0600

    Stable version

--- new_classifier.py
+++ new_classifier.py
@@ -3,7 +3,7 @@ from PIL import Image
 import cv2 as cv
 import numpy
 import os 
-
+import itertools
 
 files = os.listdir('./images')
 
@@ -13,21 +13,24 @@ labels = []
 # Features of the fruit: weight and texture
 features =  []
 
-pil_image1 = Image.open("./images/Apple1.jpg")
+example = []
+pil_image1 = Imag
+e.open("./images/Orange1.jpg")
 opencvImage1 = cv.cvtColor(numpy.array(pil_image1), cv.COLOR_BGR2GRAY)
-gray_image1 = opencvImage1.reshape(-1, opencvImage1.shape[-1])   
-
-example = gray_image1
-print(example)
+gray_image1 = opencvImage1.reshape(-1, opencvImage1.shape[-1])  
+gray_image1 = list(itertools.chain(*gray_image1)) 
+example.append(gray_image1)
+#print(example)
 for item in files:
     if item[0] == 'A':
-        labels.append('apple')
+        labels.append("Apple")
     else:
-        labels.append('Orange')
+        labels.append("Orange")
     
     pil_image = Image.open(f"./images/{item}")
     opencvImage = cv.cvtColor(numpy.array(pil_image), cv.COLOR_BGR2GRAY)
-    gray_image = opencvImage.reshape(1,-1)   
+    gray_image = opencvImage.reshape(1,-1)
+    gray_image = list(itertools.chain(*gray_image)) 
     features.append(gray_image)
 
 
@@ -35,7 +38,7 @@ for item in files:
 clf = tree.DecisionTreeClassifier()
 
 # Train the classifier on the features and labels
-clf = clf.fit(features, labels)
 
+clf = clf.fit(features, labels)
 # Predict the class of a new fruit with weight 160g and texture 0 (orange)
 print(clf.predict(example))
