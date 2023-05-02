44709eda3f55f4bbb283be973ddd8d8b7c561f7f
shiro2568 <shiro_navalny01@mail.ru>
2023-05-03 03:56:32 +0600

    Stable version - added comments

--- new_classifier.py
+++ new_classifier.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import numpy
 import os 
 import itertools
-
+# <<---  --->>      ==>     Superfluous calculation
 files = os.listdir('./images')
 
 #print(files)
@@ -12,25 +12,30 @@ files = os.listdir('./images')
 labels = []
 # Features of the fruit: weight and texture
 features =  []
-
+# An example of how a neural network works
 example = []
-pil_image1 = Imag
-e.open("./images/Orange1.jpg")
+# Example image for neural network 
+pil_image1 = Image.open("./images/Orange2.jpg")
+# Convert to number-array 
 opencvImage1 = cv.cvtColor(numpy.array(pil_image1), cv.COLOR_BGR2GRAY)
-gray_image1 = opencvImage1.reshape(-1, opencvImage1.shape[-1])  
-gray_image1 = list(itertools.chain(*gray_image1)) 
+#gray_image1 = opencvImage1.reshape(-1, opencvImage1.shape[-1])  
+# Unpacking array
+gray_image1 = list(itertools.chain(*opencvImage1)) 
+# Append result into example variable
 example.append(gray_image1)
 #print(example)
 for item in files:
+    # <<---  Adding responses for a neural network   --->>
     if item[0] == 'A':
         labels.append("Apple")
     else:
         labels.append("Orange")
-    
+    # An array of training examples 
     pil_image = Image.open(f"./images/{item}")
     opencvImage = cv.cvtColor(numpy.array(pil_image), cv.COLOR_BGR2GRAY)
-    gray_image = opencvImage.reshape(1,-1)
-    gray_image = list(itertools.chain(*gray_image)) 
+    #<<---   gray_image = opencvImage.reshape(1,-1)   --->> 
+    # Unpacking array
+    gray_image = list(itertools.chain(*opencvImage)) 
     features.append(gray_image)
 
 
@@ -38,7 +43,6 @@ for item in files:
 clf = tree.DecisionTreeClassifier()
 
 # Train the classifier on the features and labels
-
 clf = clf.fit(features, labels)
 # Predict the class of a new fruit with weight 160g and texture 0 (orange)
 print(clf.predict(example))
